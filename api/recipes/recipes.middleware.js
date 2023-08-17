84aacd253c365e03ff31ae5dd82d3d05fa3bd8e3
Matt Phillips <mattcphillips17@gmail.com>
2023-08-16 18:32:22 -0700

    middleware written

--- api/recipes/recipes.middleware.js
+++ api/recipes/recipes.middleware.js
@@ -1,8 +1,18 @@
-const Recipe = require('./recipes-model')
+const db = require('../../data/db-config')
 
 const validateRecipeID = (req, res, next) => {
-  console.log('validate ID middleware')
-  next()
+  db('recipes').where('recipe_id', req.params.recipe_id).first()
+    .then(exists => {
+      console.log(exists)
+      if(!exists) {
+        next({
+          message: `recipe with recipe_id ${req.params.recipe_id} not found`
+        })
+      } else {
+        next()
+      }
+    })
+    .catch(next)
 }
 
 module.exports = { validateRecipeID }
