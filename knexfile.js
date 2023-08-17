e3d8f32cae576193835517be93c70830133200cb
Matt Phillips <mattcphillips17@gmail.com>
2023-08-16 17:00:11 -0700

    wip

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,20 @@
-// Update with your config settings.
+const sharedConfig = {
+  client: 'sqlite3',
+  migrations: { directory: './data/migrations' },
+  seeds: { directory: './data/seeds' },
+  // sqlite3-specific configurations
+  useNullAsDefault: true,
+  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+}
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    ...sharedConfig,
+    connection: { filename: './data/cook_book.db3' },
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  testing: {
+    ...sharedConfig,
+    connection: { filename: './data/cook_book.test.db3' },
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+  production: {}
+}
