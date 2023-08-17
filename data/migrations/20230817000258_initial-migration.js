9662f76d64dff77b4cb3c9a0b84b2b1061bf7028
Matt Phillips <mattcphillips17@gmail.com>
2023-08-16 17:31:32 -0700

    migrations complete

--- data/migrations/20230817000258_initial-migration.js
+++ data/migrations/20230817000258_initial-migration.js
@@ -2,16 +2,43 @@
 exports.up = async function(knex) {
   await knex.schema
     .createTable('recipes', tbl => {
-      tbl.increments()  
+      tbl.increments('recipe_id')
+      tbl.string('recipe_name', 200).notNullable().unique()
     })
     .createTable('ingredients', tbl => {
-      tbl.increments()  
+      tbl.increments('ingredient_id')
+      tbl.string('ingredient_name', 200).notNullable().unique()
+      tbl.string('ingredient_unit', 50)
     })
     .createTable('steps', tbl => {
-      tbl.increments()  
+      tbl.increments('step_id')
+      tbl.string('step_instructions', 200).notNullable()
+      tbl.integer('step_number').notNullable().unsigned()
+      tbl.integer('recipe_id')
+        .notNullable()
+        .unsigned()
+        .references('recipe_id')
+        .inTable('recipes')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
     })
     .createTable('step_ingredients', tbl => {
-      tbl.increments()  
+      tbl.increments('step_ingredient_id')
+      tbl.float('quantity').notNullable()
+      tbl.integer('step_id')
+        .notNullable()
+        .unsigned()
+        .references('step_id')
+        .inTable('steps')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
+      tbl.integer('ingredient_id')
+        .notNullable()
+        .unsigned()
+        .references('ingredient_id')
+        .inTable('ingredients')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')  
     })
 };
 
