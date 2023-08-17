bdf039f97e3fa7656916905b33cc63c16e8de5c2
Matt Phillips <mattcphillips17@gmail.com>
2023-08-16 17:14:01 -0700

    wip

---
+++ data/migrations/20230817000258_initial-migration.js
@@ -0,0 +1,25 @@
+
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('recipes', tbl => {
+      tbl.increments()  
+    })
+    .createTable('ingredients', tbl => {
+      tbl.increments()  
+    })
+    .createTable('steps', tbl => {
+      tbl.increments()  
+    })
+    .createTable('step_ingredients', tbl => {
+      tbl.increments()  
+    })
+};
+
+
+exports.down = async function(knex) {
+  await knex.schema
+    .dropTableIfExists('step_ingredients')
+    .dropTableIfExists('steps')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes')
+};
